c27e56761b9eb88efad0130a1121eb77485f53fa
Martin Tomek <themolxyt@gmail.com>
2025-01-20 13:36:04 +0100

    Update MolochMaskCreator.py

--- Moloch/MaskDelivery/MolochMaskCreator.py
+++ Moloch/MaskDelivery/MolochMaskCreator.py
@@ -3,40 +3,70 @@ version 14.0 v5
 push $cut_paste_input
 Group {
  name MolochMaskCreator
- tile_color 0x272727ff
+ tile_color 0x46c7c7ff
  note_font "Bitstream Vera Sans Bold"
  note_font_color 0xffffffff
  selected true
- xpos 2040
- ypos 132
- addUserKnob {20 Settings l "Moloch Mask Creator"}
- addUserKnob {22 copy_to_monitor l "Copy Alpha to Monitor Mask" t "Copies input alpha to monitor.mask channel. Creates the channel if it doesn't exist." T "def copyToMonitorMask():\n    n = nuke.thisNode()\n\n    if not n.input(1):\n        nuke.message('Please connect a matte input!')\n        return\n\n    # Create monitor.mask if needed\n    channelName = 'monitor'\n    fullChannelName = 'monitor.mask'\n    if not nuke.exists(fullChannelName):\n        nuke.Layer(channelName, \[fullChannelName])\n\n    # Get internal copy node\n    with n:\n        copy = nuke.toNode('Copy1')\n        if copy:\n            copy\['from0'].setValue('rgba.alpha')\n            copy\['to0'].setValue(fullChannelName)\n\n    # Update appearance\n    if n\['tile_color'].value() == 0x272727ff:\n        n\['tile_color'].setValue(0x46C7C7ff)\n    else:\n        n\['tile_color'].setValue(0x272727ff)\n\ncopyToMonitorMask()" +STARTLINE}
+ xpos 276
+ ypos -93
+ addUserKnob {20 Settings l "Screen Mattes"}
+ addUserKnob {26 info l "" +STARTLINE T "This node copies alpha channels from inputs to dedicated matte channels.\nConnect screen mattes to inputs 1 and 2.\n\nOutputs:\n- matte_screen01.a (from input 1)\n- matte_screen02.a (from input 2)"}
+ addUserKnob {26 ""}
+ addUserKnob {26 credits l "" +STARTLINE T "Created by Martin Tomek"}
 }
  Input {
   inputs 0
-  name Matte
-  label Matte
-  xpos -129
-  ypos 86
+  name matte_screen02
+  label matte_screen02
+  xpos -119
+  ypos 148
+  number 2
+ }
+ Input {
+  inputs 0
+  name matte_screen01
+  label matte_screen01
+  xpos -133
+  ypos 62
   number 1
  }
  Input {
   inputs 0
   name Input
   xpos 0
-  ypos 0
  }
+add_layer {matte_screen01 matte_screen01.a}
  Copy {
   inputs 2
   from0 rgba.alpha
-  to0 rgba.alpha
+  to0 matte_screen01.a
   name Copy1
   xpos 0
   ypos 86
+  disable {{"\[exists parent.input1] ? 0 : 1"}}
+ }
+add_layer {matte_screen02 matte_screen02.a}
+ Copy {
+  inputs 2
+  from0 rgba.alpha
+  to0 matte_screen02.a
+  name Copy2
+  xpos 0
+  ypos 153
+  disable {{"\[exists parent.input2] ? 0 : 1"}}
  }
  Output {
   name Output1
   xpos 0
-  ypos 196
+  ypos 306
+ }
+ Viewer {
+  frame 1041
+  frame_range 986-1124
+  fps 25
+  viewerProcess "sRGB (ACES)"
+  name Viewer1
+  xpos 0
+  ypos 232
  }
 end_group
